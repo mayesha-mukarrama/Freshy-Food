31b959d46645ddc172d93adf918058565af3019c
mayesha <mayesha.mukarrama.cse@ulab.edu.bd>
2018-08-17 00:43:56 +0600

    update

---
+++ Freshy Food/login_form.php
@@ -0,0 +1,170 @@
+<?php
+#this is Login form page , if user is already logged in then we will not allow user to access this page by executing isset($_SESSION["uid"])
+#if below statment return true then we will send user to their profile.php page
+if (isset($_SESSION["uid"])) {
+	header("location:profile.php");
+}
+//in action.php page if user click on "ready to checkout" button that time we will pass data in a form from action.php page
+if (isset($_POST["login_user_with_product"])) {
+	//this is product list array
+	$product_list = $_POST["product_id"];
+	//here we are converting array into json format because array cannot be store in cookie
+	$json_e = json_encode($product_list);
+	//here we are creating cookie and name of cookie is product_list
+	setcookie("product_list",$json_e,strtotime("+1 day"),"/","","",TRUE);
+
+}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>FreshyFood</title>
+		<link rel="stylesheet" href="css/bootstrap.min.css"/>
+		<script src="js/jquery2.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="main.js"></script>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<link rel="shortcut icon" href="freshyFood.ico">
+		<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript">
+$(function() {
+    $(this).bind("contextmenu", function(e) {
+        e.preventDefault();
+    });
+}); 
+</script>
+<script type="text/JavaScript"> 
+    function killCopy(e){ return false } 
+    function reEnable(){ return true } 
+    document.onselectstart=new Function ("return false"); 
+    if (window.sidebar)
+    { 
+        document.onmousedown=killCopy; 
+        document.onclick=reEnable; 
+    } 
+	
+document.onkeydown = function(e) {
+if(event.keyCode == 123) {
+return false;
+}
+if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
+return false;
+}
+if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
+return false;
+}
+if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
+return false;
+}
+}
+</script>
+	</head>
+<body oncontextmenu="return false;">
+<div class="wait overlay">
+	<div class="loader"></div>
+</div>
+	<div class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container-fluid">	
+			<div class="navbar-header">
+				<a href="index.php" class="navbar-brand" style="font-family:Jokerman;color:white">FreshyFood</a>
+			</div>
+		<div class="collapse navbar-collapse" id="collapse">
+			<ul class="nav navbar-nav">
+				 
+				<li style="width:230px;left:10px;top:10px;"><input type="text" class="form-control" id="search" placeholder="Search"></li>
+				<li style="top:10px;left:20px;"><button class="btn btn-primary" id="search_btn">Search</button></li>
+			</ul>
+			
+			<ul class="nav navbar-nav navbar-right">
+			 
+			    <li><a href="#">&nbsp;En | বাং</a></li>
+				<li><a href="phome.php"><span class="glyphicon glyphicon-home"></span>&nbsp;Product</a></li>				
+			    <li><a href="#"><span class="glyphicon glyphicon-envelope"></span>&nbsp;Contact Us</a></li>
+				</div>
+		</div>
+	</div>
+	<p><br/></p>
+	<p><br/></p>
+	<p><br/></p>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-2"></div>
+			<div class="col-md-8" id="signup_msg">
+				<!--Alert from signup form-->
+			</div>
+			<div class="col-md-2"></div>
+		</div>
+		<div class="row">
+			<div class="col-md-4"></div>
+			<div class="col-md-4">
+				<div class="panel panel-primary">
+					<div class="panel-heading">Customer Login Form</div>
+					<div class="panel-body">
+						<!--User Login Form-->
+						<form onsubmit="return false" id="login">
+							<label for="email">Email</label>
+							<input type="email" class="form-control" name="email" id="email" required/>
+							<label for="email">Password</label>
+							<input type="password" class="form-control" name="password" id="password" required/>
+							<p><br/></p>
+							<a href="#" style="color:#333; list-style:none;">Forgotten Password</a><input type="submit" class="btn btn-success" style="float:right;" Value="Login">
+							<!--If user dont have an account then he/she will click on create account button-->
+							<div><a href="customer_registration.php?register=1">Create a new account?</a></div>						
+						</form>
+				</div>
+				<div class="panel-footer"><center><img src="copyr.png" height="15px" width="15px">&nbsp;<img src="freshyFood.ico" height="15px" width="15px">&nbsp;FreshyFood&nbsp;All rights reserved</center><div id="e_msg"></div></div>
+			</div>
+		</div>
+		<div class="col-md-4"></div>
+	</div>
+	
+	 <p><br/></p>
+	<p><br/></p>
+	<p><br/></p>
+				 <!-------------------------------------footer------------------------------------------------------->
+<div class="panel-footer">
+								
+  <ul style="list-style-type:none" >
+ 
+   <center>
+  <li><style="text-decoration:none"><h4>Our Services:</h4></li>
+  <li><a href="formalin.php" style="text-decoration:none">Live formalin detection</a></li></li>
+  <li><a href="contact_us.php" style="text-decoration:none">Contact Us</a></li></li>
+  <li><a href="#" style="text-decoration:none">FAQ</a></li></li>
+  <li><a href="about_us.php" style="text-decoration:none">About Us</a></li></li>
+  </br>
+   <li><style="text-decoration:none"><h4>Payment Options</h4></li></br>
+  <li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style="font-size: 10px; font-weight:bold; display: inline-flex;vertical-align: super; width: 50px">CASH ON DELIVERY</span>&nbsp;&nbsp;<img src="slide_images/bkash.png">&nbsp;&nbsp;<img src="slide_images/rocket.png">&nbsp;&nbsp;<img src="slide_images/ucash.png">  </li>
+  </center>
+  </div>
+  <div class="panel-footer"><center><img src="copyr.png" height="15px" width="15px">&nbsp;<img src="freshyFood.ico" height="15px" width="15px">&nbsp;Freshy Food&nbsp;All rights reserved</center></div>
+</div>
+	
+	
+	
+	
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
