9677e89060cf918b2f5f4bb6aa63a5c96732adc4
mayesha <mayesha.mukarrama.cse@ulab.edu.bd>
2018-08-20 23:59:13 +0600

    update

---
+++ Freshy Food/about_us.php
@@ -0,0 +1,380 @@
+<!DOCTYPE html>
+<html>
+	<head>
+        <meta charset="UTF-8">
+		<title>FreshyFood</title>
+		<link rel="shortcut icon" href="freshyFood.ico">
+		<link rel="stylesheet" href="css/bootstrap.min.css"/>
+		<script src="js/jquery2.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/main.js"></script>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<script type="text/javascript" src="js/jquery.min.js"></script>
+		 
+
+  <title>About us</title>
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css">
+  
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/dropdown/css/style.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+		<style>
+		
+		.button {
+    background-color: #4CAF50; /* Green */
+    border: none;
+    color: white;
+    padding: 10px 30px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+}
+</style>
+		</head>
+		<body oncontextmenu="return false;">
+<div class="wait overlay">
+	<div class="loader"></div>
+</div>
+	<div class="navbar navbar-inverse navbar-fixed-top">
+	<div class="card">
+		<div class="container-fluid">	
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse" aria-expanded="false">
+					<span class="sr-only">navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a href="index.php" class="navbar-brand" style="font-family:Jokerman;color:white">FreshyFood</a>
+			</div>
+		<div class="collapse navbar-collapse" id="collapse">
+			<ul class="nav navbar-nav">
+				 
+				<li style="width:230px;left:10px;top:10px;"><input type="text" class="form-control" id="search" placeholder="Search"></li>
+				<li style="left:10px;top:5px; bottom: 10px"><button class="btn btn-primary" id="search_btn"><font size="2">Search</button></li>
+			</ul>
+			
+			<ul class="nav navbar-nav navbar-right">
+			 
+			    <li style="font-size:14px;"><a href="#">&nbsp;En | বাং</a></li>
+				<li style="font-size:14px;"><a href="phome.php"><span class="glyphicon glyphicon-home"></span>&nbsp;Product</a></li>				
+			    <li style="font-size:14px;"><a href="#"><span class="glyphicon glyphicon-envelope"></span>&nbsp;Contact Us</a></li>
+				<li style="font-size:14px;"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-shopping-cart"></span>&nbsp;Cart&nbsp;<span class="badge">0</span></a>
+					<div class="dropdown-menu" style="width:400px;">
+						<div class="panel panel-success">
+							<div class="panel-heading">
+								<div class="row">
+									<div class="col-md-3">Sl.No</div>
+									<div class="col-md-3">Product Image</div>
+									<div class="col-md-3">Product Name</div>
+									<div class="col-md-3">Price in Tk.</div>
+								</div>
+							</div>
+							<div class="panel-body">
+								<div id="cart_product">
+								
+								</div>
+							</div>
+							<div class="panel-footer"></div>
+						</div>
+					</div>
+				</li>
+				<li style="font-size:14px;"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user"></span>&nbsp;SignIn</a>
+					<ul class="dropdown-menu">
+						<div style="width:300px;">
+							<div class="panel panel-primary">
+								<div class="panel-heading">Login</div>
+								<div class="panel-heading">
+									<form onsubmit="return false" id="login">
+										<label for="email">Email</label>
+										<input type="email" class="form-control" name="email" id="email" required/>
+										<label for="email">Password</label>
+										<input type="password" class="form-control" name="password" id="password" required/>
+										<p><br/></p>
+										<a href="#" style="color:white; list-style:none;">Forgotten Password</a><input type="submit" class="btn btn-success" style="float:right;">
+									</form>
+								</div>
+								<div class="panel-footer" id="e_msg"></div>
+							</div>
+						</div>
+					</ul>
+				</li>
+			</ul>
+		</div>
+	</div>
+	
+ </div>
+</div> 
+
+<!----------------------//header--------------------------------------------->
+
+        <img src="slide_images/about.png"  style="width:100%; height:420px;">
+		<center><a href="#" class="button">Invest us now</a><a href="#" class="button">Our partners</a></center>
+
+<section class="engine"><a href="https://mobiri.se/i">free web creation software</a></section><section class="accordion2 cid-r14yTIKd84" id="accordion2-f">
+
+
+	              <div class="container">
+        <div class="media-container-row pt-5">
+            <div class="accordion-content">
+                <h1 class="mbr-section-title align-center pb-3 mbr-fonts-style display-5"><font size="5"><strong><br></strong><strong>Our offerings</strong></h1>
+                <br>
+			
+                <div id="bootstrap-accordion_0" class="panel-group accordionStyles accordion pt-5 mt-3" role="tablist" aria-multiselectable="true">
+                        <div class="card">
+                            <div class="card-header" role="tab" id="headingOne">
+                                <a role="button" class="collapsed panel-title text-black" data-toggle="collapse" data-core="" href="#collapse1_0" aria-expanded="false" aria-controls="collapse1">
+                                    <h4 class="mbr-fonts-style display-5">
+                                        <span class="sign mbr-iconfont mbri-arrow-down inactive"></span>&nbsp;Live formalin detection</h4>
+                                </a>
+                            </div>
+                            <div id="collapse1_0" class="panel-collapse noScroll collapse" role="tabpanel" aria-labelledby="headingOne" data-parent="#bootstrap-accordion_0">
+                                <div class="panel-body p-4">
+                                    <p class="mbr-fonts-style panel-text display-7"><font size="4">To ensure you about your product freshness is our top priority. Formalin detection test will be taken in front of you.</p>
+                                </div>
+                            </div>
+                        </div>
+                
+                        <div class="card">
+                            <div class="card-header" role="tab" id="headingTwo">
+                                <a role="button" class="collapsed panel-title text-black" data-toggle="collapse" data-core="" href="#collapse2_0" aria-expanded="false" aria-controls="collapse2">
+                                    <h4 class="mbr-fonts-style mbr-fonts-style display-5">
+                                        <span class="sign mbr-iconfont mbri-arrow-down inactive"></span>&nbsp;Gift baskets</h4>
+                                </a>
+                                
+                            </div>
+                            <div id="collapse2_0" class="panel-collapse noScroll collapse" role="tabpanel" aria-labelledby="headingTwo" data-parent="#bootstrap-accordion_0">
+                                <div class="panel-body p-4">
+                                    <p class="mbr-fonts-style panel-text display-7"><font size="4">We have curated the tastiest collections of fresh gourmet gift baskets featuring the most beautiful fruit of our orchards paired with gourmet treats for all occasions including corporate gifts, get well soon, and birthday baskets. Our baskets are guaranteed to arrive fresh and delicious. </p>
+                                </div>
+                            </div>
+                        </div>
+                
+                        <div class="card">
+                            <div class="card-header" role="tab" id="headingThree">
+                                <a role="button" class="collapsed panel-title text-black" data-toggle="collapse" data-core="" href="#collapse3_0" aria-expanded="false" aria-controls="collapse3">
+                                    <h4 class="mbr-fonts-style display-5">
+                                        <span class="sign mbr-iconfont mbri-arrow-down inactive"></span>&nbsp;Delivery anywhere, anytime</h4>
+                                </a>
+                            </div>
+                            <div id="collapse3_0" class="panel-collapse noScroll collapse" role="tabpanel" aria-labelledby="headingThree" data-parent="#bootstrap-accordion_0">
+                                <div class="panel-body p-4">
+                                    <p class="mbr-fonts-style panel-text display-7"><font size="4">We will serve you in any occasions, office, events, birthday parties, ceremonies with low price so that you can afford.</p>
+                                </div>
+                            </div>
+                        </div>
+						<div class="card">
+                            <div class="card-header" role="tab" id="headingThree">
+                                <a role="button" class="collapsed panel-title text-black" data-toggle="collapse" data-core="" href="#collapse3_0" aria-expanded="false" aria-controls="collapse3">
+                                    <h4 class="mbr-fonts-style display-5">
+                                        <span class="sign mbr-iconfont mbri-arrow-down inactive"></span>&nbsp;Verified suppliers</h4>
+                                </a>
+                            </div>
+                            <div id="collapse3_0" class="panel-collapse noScroll collapse" role="tabpanel" aria-labelledby="headingThree" data-parent="#bootstrap-accordion_0">
+                                <div class="panel-body p-4">
+                                    <p class="mbr-fonts-style panel-text display-7"><font size="4">Every time we take any of our service partner on our marketplace, they have to go through a 2 step verification of their company and their employees.</p>
+                                </div>
+                            </div>
+                        </div>
+						<div class="card">
+                            <div class="card-header" role="tab" id="headingThree">
+                                <a role="button" class="collapsed panel-title text-black" data-toggle="collapse" data-core="" href="#collapse3_0" aria-expanded="false" aria-controls="collapse3">
+                                    <h4 class="mbr-fonts-style display-5">
+                                        <span class="sign mbr-iconfont mbri-arrow-down inactive"></span>&nbsp;Creating Value</h4>
+                                </a>
+                            </div>
+                            <div id="collapse3_0" class="panel-collapse noScroll collapse" role="tabpanel" aria-labelledby="headingThree" data-parent="#bootstrap-accordion_0">
+                                <div class="panel-body p-4">
+                                    <p class="mbr-fonts-style panel-text display-7"><font size="4">At Freshy Food, our approach to social impact is a natural extension of our vision to become the world’s most respected food brand. In a world of social change and environmental imperatives, a brand that stands for trust, health and fresh is more important than ever.</p>
+                                </div>
+                            </div>
+                        </div>
+                
+                        
+                
+                        
+                
+                        
+                </div>
+            </div>
+			
+            <div class="mbr-figure" style="width: 100%; height: 50%";>
+                    <img src="assets/images/mbr-1042x901.jpg" alt="Mobirise" title="">
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="features11 cid-r14DrwwTTx" id="features11-l">
+
+    
+
+    
+
+    <div class="container">   
+        <div class="col-md-12">
+            <div class="media-container-row">
+                <div class="mbr-figure" style="width: 50%;">
+                    <img src="assets/images/mbr-952x634.jpg" alt="Mobirise" title="">
+                </div>
+				<center>
+                <div class=" align-left aside-content">
+				</br>
+				</br>
+				
+                    <h2 class="mbr-title pt-2 mbr-fonts-style display-2"><strong>A team to serve you a healthier life</strong></h2>
+					</br>
+                    <div class="mbr-section-text">
+                        <p class="mbr-text mb-5 pt-3 mbr-light mbr-fonts-style display-5"></p>
+                </div>
+				
+
+                    <div class="block-content">
+                        <div class="card p-3 pr-3">
+                            <div class="media">
+                                     
+                                <div class="media-body">
+									<h4 class="mbr-section-subtitle mbr-light mbr-fonts-style display-5">
+              At the heart of our company is our dedicated team of passionate and tech-savvy enthusiasts, who come to work every day with the mission of catering the best possible services across the country by providing the freshest, organic and most delicious food to meet your desire..!!!
+                    </h4>
+                                </div>
+                            </div>                
+</center>
+                            <div class="card-box">
+                                <p class="block-text mbr-fonts-style display-7">. 
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="card p-3 pr-3">
+                            <div class="media">
+                          
+                                <div class="media-body">
+                                    <h4 class="card-title mbr-fonts-style display-7"></h4>
+                                </div>
+                            </div>                
+
+                            <div class="card-box">
+                                <p class="block-text mbr-fonts-style display-7"></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> 
+    </div>          
+</section>
+
+<section class="clients cid-r14GwYrVzI" id="clients-m">
+  <div class="container mb-5">
+            <div class="media-container-row">
+                <div class="col-12 align-center">
+                    <h2 class="mbr-section-title pb-3 mbr-fonts-style display-2">
+                        Our Partners
+                    </h2>
+                    <h3 class="mbr-section-subtitle mbr-light mbr-fonts-style display-5"><strong>We try to supply the best thing for you</strong></h3>
+                </div>
+            </div>
+        </div>
+
+    <div class="container">
+   
+<div class="center">
+				
+  
+                    
+     <div class="center">
+               
+		<div class="row">	
+		<div class="column">
+    <img src="assets/images/1.png"   class="center" hspace="10">
+  </div>
+  	<div class="column">
+    <img src="assets/images/2.png"   class="center" hspace="10">
+  </div> 
+  	<div class="column">
+    <img src="assets/images/3.png"   class="center" hspace="10">
+  </div>
+  <div class="column">
+    <img src="assets/images/4.png"   class="center" hspace="10">
+  </div>
+ 	<div class="column">
+    <img src="assets/images/5.png"   class="center" hspace="10">
+  </div>
+  		<div class="column">
+    <img src="assets/images/1.png"   class="center" hspace="10">
+  </div>
+  	<div class="column">
+    <img src="assets/images/2.png"   class="center" hspace="10">
+  </div>
+  		<div class="column">
+    <img src="assets/images/3.png"   class="center" hspace="10">
+  </div>
+  </div>
+  <center/>
+  
+                        </div>
+                    </div>
+                </div></div>
+  
+             
+            </div>
+        </div>
+    </div>
+</section>
+  
+  
+
+
+  
+  
+
+
+		 
+
+
+		 <!-------------------------------------footer------------------------------------------------------->
+<div class="card">
+
+<div class="panel-footer">
+								
+  <ul style="list-style-type:none" >
+  <center>
+  <div class="row">
+			
+    <div class="col-md-3" >
+  <li><style="text-decoration:none"><h4>Our Services:</h4></li>
+  <li><a href="formalin.php" style="text-decoration:none">Live formalin detection</a></li></li>
+  <li><a href="contact_us.php" style="text-decoration:none">Contact Us</a></li></li>
+  <li><a href="faq.php" style="text-decoration:none">FAQ</a></li></li>
+  <li><a href="about_us.php" style="text-decoration:none">About Us</a></li></li>
+  </br>
+  </div>
+   <div class="col-md-3" >
+  <li><style="text-decoration:none"><h4>Our Products:</h4></li>
+  <li><a href="phome.php" style="text-decoration:none">Fruits</a></li>
+  <li><a href="phome.php" style="text-decoration:none">Vegetables</a></li>
+  <li><a href="phome.php" style="text-decoration:none">Baverages</a></li>
+  <li><a href="phome.php" style="text-decoration:none">Gift Basket</a></li>
+  <li><a href="phome.php" style="text-decoration:none">Dairy</a></li>
+  <li><a href="phome.php" style="text-decoration:none">Packages</a></li>
+  </br>
+  </div>
+  <div class="col-md-6" >
+   <li><style="text-decoration:none"><h4>Payment Options:</h4></li></br>
+  <li><span style="font-size: 10px; font-weight:bold; display: inline-flex;vertical-align: super; width: 50px">CASH ON DELIVERY</span>&nbsp;&nbsp;<img src="slide_images/bkash.png">&nbsp;&nbsp;<img src="slide_images/rocket.png">&nbsp;&nbsp;<img src="slide_images/ucash.png">  </li>
+  </center>
+  </div>
+  </div>
+  </div>
+  <div class="panel-footer"><center><img src="copyr.png" height="15px" width="15px">&nbsp;<img src="freshyFood.ico" height="15px" width="15px">&nbsp;Freshy Food&nbsp;All rights reserved</center></div>
+</div>
+
+	
+</body>
+</html>
