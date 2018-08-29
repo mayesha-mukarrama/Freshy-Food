3bc8b65c3b5aca0afb0c5ba79d9c8c9b61e312fc
mayesha <mayesha.mukarrama.cse@ulab.edu.bd>
2018-08-30 00:46:03 +0600

    faq

---
+++ Freshy Food/faq.php
@@ -0,0 +1,242 @@
+<!doctype html>
+<html lang="en" class="no-js">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
+
+	<link rel="stylesheet" href="faq/css/reset.css"> <!-- CSS reset -->
+	<link rel="stylesheet" href="faq/css/style.css"> <!-- Resource style -->
+	<script src="faq/js/modernizr.js"></script> <!-- Modernizr -->
+	<title>FAQ </title>
+</head>
+<body>
+<header>
+	<h1>FAQs of Freshy Food</h1>
+</header>
+<section class="cd-faq">
+	<ul class="cd-faq-categories">
+		<li><a class="selected" href="#basics">General</a></li>
+		<li><a href="#mobile">Products</a></li>
+		<li><a href="#account">Formalin</a></li>
+		<li><a href="#payments">Payments</a></li>
+		<li><a href="#privacy">Policy</a></li>
+		<li><a href="#delivery">Delivery</a></li>
+	</ul> <!-- cd-faq-categories -->
+
+	<div class="cd-faq-items">
+		<ul id="basics" class="cd-faq-group">
+			<li class="cd-faq-title"><h2>Basics</h2></li>
+			<li>
+				<a class="cd-faq-trigger" href="#0">How does the site work?</a>
+				<div class="cd-faq-content">
+					<p>You can browse the site or use our search engine to find your desired products. You can then add them to your shopping cart and click on place order. You let us know your address you are done.  A Freshy Food representative will then deliver your order right to your home or office.</p>
+				</div> <!-- cd-faq-content -->
+			</li>
+
+			<li>
+				<a class="cd-faq-trigger" href="#0">How do I know when my order is here?</a>
+				<div class="cd-faq-content">
+					<p> </p>
+                </div					<!-- cd-faq-content -->
+			</li>
+
+			<li>
+				<a class="cd-faq-trigger" href="#0">Can I remove a post?</a>
+				<div class="cd-faq-content">
+					<p> </p>
+                </div <!-- cd-faq-content -->
+			</li>
+
+			<li>
+				<a class="cd-faq-trigger" href="#0">How do reviews work?</a>
+				<div class="cd-faq-content">
+					<p> </p>
+                </div <!-- cd-faq-content -->
+			</li>
+		</ul> <!-- cd-faq-group -->
+
+		<ul id="mobile" class="cd-faq-group">
+			<li class="cd-faq-title"><h2>Mobile</h2></li>
+			<li>
+				<a class="cd-faq-trigger" href="#0">How does syncing work?</a>
+				<div class="cd-faq-content">
+					<p> </p>
+                </div<!-- cd-faq-content -->
+			</li>
+
+			<li>
+				<a class="cd-faq-trigger" href="#0">How do I upload files from my phone or tablet?</a>
+				<div class="cd-faq-content">
+					<p> </p>
+                </div <!-- cd-faq-content -->
+			</li>
+
+			<li>
+				<a class="cd-faq-trigger" href="#0">How do I link to a file or folder?</a>
+				<div class="cd-faq-content">
+					<p> </p>
+                </div <!-- cd-faq-content -->
+			</li>
+		</ul> <!-- cd-faq-group -->
+
+		<ul id="account" class="cd-faq-group">
+			<li class="cd-faq-title"><h2>Account</h2></li>
+			<li>
+				<a class="cd-faq-trigger" href="#0">How do I change my password?</a>
+				<div class="cd-faq-content">
+					<p> </p>
+                </div<!-- cd-faq-content -->
+			</li>
+
+			<li>
+				<a class="cd-faq-trigger" href="#0">How do I delete my account?</a>
+				<div class="cd-faq-content">
+					<p> </p>
+                </div<!-- cd-faq-content -->
+			</li>
+
+			<li>
+				<a class="cd-faq-trigger" href="#0">How do I change my account settings?</a>
+				<div class="cd-faq-content">
+					<p> </p>
+                </div<!-- cd-faq-content -->
+			</li>
+
+			<li>
+				<a class="cd-faq-trigger" href="#0">I forgot my password. How do I reset it?</a>
+				<div class="cd-faq-content">
+					<p> </p>
+                </div <!-- cd-faq-content -->
+			</li>
+		</ul> <!-- cd-faq-group -->
+
+		<ul id="payments" class="cd-faq-group">
+			<li class="cd-faq-title"><h2>Payments</h2></li>
+			<li>
+				<a class="cd-faq-trigger" href="#0">Can I have an invoice for my subscription?</a>
+				<div class="cd-faq-content">
+					<p> </p>
+                </div <!-- cd-faq-content -->
+			</li>
+
+			<li>
+				<a class="cd-faq-trigger" href="#0">Why did my credit card or PayPal payment fail?</a>
+				<div class="cd-faq-content">
+					<p> </p>
+                </div<!-- cd-faq-content -->
+			</li>
+
+			<li>
+				<a class="cd-faq-trigger" href="#0">Why does my bank statement show multiple charges for one upgrade?</a>
+				<div class="cd-faq-content">
+					<p> </p>
+                </div<!-- cd-faq-content -->
+			</li>
+		</ul> <!-- cd-faq-group -->
+
+		<ul id="privacy" class="cd-faq-group">
+			<li class="cd-faq-title"><h2>Privacy</h2></li>
+			<li>
+				<a class="cd-faq-trigger" href="#0">Can I specify my own private key?</a>
+				<div class="cd-faq-content">
+					<p> </p>
+                </div>!-- cd-faq-content -->
+			</li>
+
+			<li>
+				<a class="cd-faq-trigger" href="#0">My files are missing! How do I get them back?</a>
+				<div class="cd-faq-content">
+					<p> </p>
+                </div> <!-- cd-faq-content -->
+			</li>
+
+			<li>
+				<a class="cd-faq-trigger" href="#0">How can I access my account data?</a>
+				<div class="cd-faq-content">
+					<p> </p>
+                </div> <!-- cd-faq-content -->
+			</li>
+
+			<li>
+				<a class="cd-faq-trigger" href="#0">How can I control if other search engines can link to my profile?</a>
+				<div class="cd-faq-content">
+					<p> </p>
+                </div> <!-- cd-faq-content -->
+			</li>
+		</ul> <!-- cd-faq-group -->
+
+		<ul id="delivery" class="cd-faq-group">
+			<li class="cd-faq-title"><h2>Delivery</h2></li>
+			<li>
+				<a class="cd-faq-trigger" href="#0">What should I do if my order hasn't been delivered yet?</a>
+				<div class="cd-faq-content">
+					<p> </p>
+                </div> <!-- cd-faq-content -->
+			</li>
+
+			<li>
+				<a class="cd-faq-trigger" href="#0">How can I find your international delivery information?</a>
+				<div class="cd-faq-content">
+					<p> </p>
+                </div> <!-- cd-faq-content -->
+			</li>
+
+			<li>
+				<a class="cd-faq-trigger" href="#0">Who takes care of shipping?</a>
+				<div class="cd-faq-content">
+					<p> </p>
+                </div>  <!-- cd-faq-content -->
+			</li>
+
+			<li>
+				<a class="cd-faq-trigger" href="#0">How do returns or refunds work?</a>
+				<div class="cd-faq-content">
+					<p> </p>
+                </div>  <!-- cd-faq-content -->
+			</li>
+
+			<li>
+				<a class="cd-faq-trigger" href="#0">How do I use shipping profiles?</a>
+				<div class="cd-faq-content">
+					<p> </p>
+                </div>  <!-- cd-faq-content -->
+			</li>
+
+			<li>
+				<a class="cd-faq-trigger" href="#0">How does your UK Next Day Delivery service work?</a>
+				<div class="cd-faq-content">
+					<p> </p>
+                </div>  <!-- cd-faq-content -->
+			</li>
+
+			<li>
+				<a class="cd-faq-trigger" href="#0">How does your Next Day Delivery service work?</a>
+				<div class="cd-faq-content">
+					<p> </p>
+                </div>  <!-- cd-faq-content -->
+			</li>
+
+			<li>
+				<a class="cd-faq-trigger" href="#0">When will my order arrive?</a>
+				<div class="cd-faq-content">
+					<p> </p>
+                </div>  <!-- cd-faq-content -->
+			</li>
+
+			<li>
+				<a class="cd-faq-trigger" href="#0">When will my order ship?</a>
+				<div class="cd-faq-content">
+					<p> </p>
+                </div> <!-- cd-faq-content -->
+			</li>
+		</ul> <!-- cd-faq-group -->
+	</div> <!-- cd-faq-items -->
+	<a href="#0" class="cd-close-panel">Close</a>
+</section> <!-- cd-faq -->
+<script src="js/jquery-2.1.1.js"></script>
+<script src="js/jquery.mobile.custom.min.js"></script>
+<script src="js/main.js"></script> <!-- Resource jQuery -->
+</body>
+</html>
