c91f674f03a9fe80f58b4d4e66d913ce39427714
mayesha <mayesha.mukarrama.cse@ulab.edu.bd>
2018-08-31 21:19:21 +0600

    customer profile

--- Freshy Food/profile.php
+++ Freshy Food/profile.php
@@ -1,10 +1,4 @@
-<?php
 
-session_start();
-if(!isset($_SESSION["uid"])){
-	header("location:index.php");
-}
-?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -92,12 +86,7 @@ return false;
 							</div>
 							<div class="panel-body">
 								<div id="cart_product">
-								<!--<div class="row">
-									<div class="col-md-3">Sl.No</div>
-									<div class="col-md-3">Product Image</div>
-									<div class="col-md-3">Product Name</div>
-									<div class="col-md-3">Price in Tk</div>
-								</div>-->
+								
 								</div>
 							</div>
 							<div class="panel-footer"></div>
@@ -163,89 +152,6 @@ return false;
 		</div>
 	</div>
 	
-	<!-------------------------------------footer------------------------------------------------------->
-
-
-<div class="card">
-
-<div class="panel-footer">
-								
-  <ul style="list-style-type:none" >
-  <center>
-  <div class="row">
-			
-    <div class="col-md-3" >
-  <li><style="text-decoration:none"><h4>Our Services:</h4></li>
-  <li><a href="formalin.php" style="text-decoration:none">Live formalin detection</a></li></li>
-  <li><a href="contact_us.php" style="text-decoration:none">Contact Us</a></li></li>
-  <li><a href="faq.php" style="text-decoration:none">FAQ</a></li></li>
-  <li><a href="about_us.php" style="text-decoration:none">About Us</a></li></li>
-  <li><a href="Supplier_Corporate.php" style="text-decoration:none">Career</a></li></li>
-  </br>
-  </div>
-   <div class="col-md-3" >
-  <li><style="text-decoration:none"><h4>Our Products:</h4></li>
-  <li><a href="phome.php" style="text-decoration:none">Fruits</a></li>
-  <li><a href="phome.php" style="text-decoration:none">Vegetables</a></li>
-  <li><a href="phome.php" style="text-decoration:none">Baverages</a></li>
-  <li><a href="phome.php" style="text-decoration:none">Gift Basket</a></li>
-  <li><a href="phome.php" style="text-decoration:none">Dairy</a></li>
-  <li><a href="phome.php" style="text-decoration:none">Packages</a></li>
-  </br>
-  </div>
-  <div class="col-md-6" >
-   <li><style="text-decoration:none"><h4>Payment Options:</h4></li></br>
-  <li><span style="font-size: 10px; font-weight:bold; display: inline-flex;vertical-align: super; width: 50px">CASH ON DELIVERY</span>&nbsp;&nbsp;<img src="slide_images/bkash.png">&nbsp;&nbsp;<img src="slide_images/rocket.png">&nbsp;&nbsp;<img src="slide_images/ucash.png">  </li>
-  </center>
-  </div>
-  </div>
-  </div>
-  <div class="panel-footer"><center><img src="copyr.png" height="15px" width="15px">&nbsp;<img src="freshyFood.ico" height="15px" width="15px">&nbsp;Freshy Food&nbsp;All rights reserved</center></div>
-</div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	
 
 
