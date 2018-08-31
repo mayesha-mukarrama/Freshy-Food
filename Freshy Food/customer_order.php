ac008f72f8a39cac3e64d3063ca6383c47a9f277
mayesha <mayesha.mukarrama.cse@ulab.edu.bd>
2018-09-01 00:09:49 +0600

    customer Order details

--- Freshy Food/customer_order.php
+++ Freshy Food/customer_order.php
@@ -1,4 +1,128 @@
+<?php
 
+session_start();
+if(!isset($_SESSION["uid"])){
+	header("location:index.php");
+}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Freshy Food</title>
+		<link rel="stylesheet" href="css/bootstrap.min.css"/>
+		<script src="js/jquery2.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="main.js"></script>
+		<link rel="shortcut icon" href="freshyFood.ico">
+		<style>
+			table tr td {padding:10px;}
+		</style>
+		<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript">
+$(function() {
+    $(this).bind("contextmenu", function(e) {
+        e.preventDefault();
+    });
+}); 
+</script>
+<script type="text/JavaScript"> 
+    function killCopy(e){ return false } 
+    function reEnable(){ return true } 
+    document.onselectstart=new Function ("return false"); 
+    if (window.sidebar)
+    { 
+        document.onmousedown=killCopy; 
+        document.onclick=reEnable; 
+    } 
+	
+document.onkeydown = function(e) {
+if(event.keyCode == 123) {
+return false;
+}
+if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
+return false;
+}
+if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
+return false;
+}
+if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
+return false;
+}
+}
+</script>
+	</head>
+<body oncontextmenu="return false;">
+	<div class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container-fluid">	
+			<div class="navbar-header">
+				<a href="index.php" class="navbar-brand" style="font-family:Jokerman;color:white">FreshyFood</a>
+			</div>
+		<div class="collapse navbar-collapse" id="collapse">
+			<ul class="nav navbar-nav">
+				 
+				<li style="width:230px;left:10px;top:10px;"><input type="text" class="form-control" id="search" placeholder="Search"></li>
+				<li style="top:10px;left:20px;"><button class="btn btn-primary" id="search_btn">Search</button></li>
+			</ul>
+			
+			<ul class="nav navbar-nav navbar-right">
+			 
+			    <li><a href="#">&nbsp;En | বাং</a></li>
+				<li><a href="phome.php"><span class="glyphicon glyphicon-home"></span>&nbsp;Product</a></li>				
+			    <li><a href="#"><span class="glyphicon glyphicon-envelope"></span>&nbsp;Contact Us</a></li>
+		</div>
+	</div>
+	<p><br/></p>
+	<p><br/></p>
+	<p><br/></p>
+	<div class="container-fluid">
+	
+		<div class="row">
+			<div class="col-md-2"></div>
+			<div class="col-md-8">
+				<div class="panel panel-default">
+					<div class="panel-heading"></div>
+					<div class="panel-body">
+						<h1>Customer Order details</h1>
+						<hr/>
+						<?php
+							include_once("db.php");
+							$user_id = $_SESSION["uid"];
+							$orders_list = "SELECT o.order_id,o.user_id,o.product_id,o.qty,o.trx_id,o.p_status,p.product_title,p.product_price,p.product_image
+							FROM orders o,products p WHERE o.user_id='$user_id' AND o.product_id=p.product_id";
+							$query = mysqli_query($con,$orders_list);
+							if (mysqli_num_rows($query) > 0) {
+								while ($row=mysqli_fetch_array($query)) {
+									?>
+										<div class="row">
+											<div class="col-md-6">
+												<img style="float:right;" src="product_images/<?php echo $row['product_image']; ?>" class="img-responsive img-thumbnail" draggable="false"/>
+											</div>
+											<div class="col-md-6">
+												<table>
+													<tr><td>Product Name</td><td><b><?php echo $row["product_title"]; ?></b> </td></tr>
+													<tr><td>Product Price</td><td><b><?php echo "Tk".$row["product_price"]; ?></b></td></tr>
+													<tr><td>Quantity</td><td><b><?php echo $row["qty"]; ?></b></td></tr>
+													<tr><td>Transaction Id</td><td><b><?php echo $row["trx_id"]; ?></b></td></tr>
+													
+												</table>
+											</div>
+											
+										</div>
+									<?php
+								}
+							}
+						?>
+						
+					</div>
+				
+				</div>
+			</div>
+			<div class="col-md-2"></div>
+		</div>
+	</div>
+
+	
 	<!-------------------------------------footer------------------------------------------------------->
 
 
