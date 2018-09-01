04f5a69d831dd9fed2ba4ad4323fce5ce8765330
mayesha-mukarrama <39920277+mayesha-mukarrama@users.noreply.github.com>
2018-09-01 23:52:10 +0600

    Update contact_us.php

--- Freshy Food/contact_us.php
+++ Freshy Food/contact_us.php
@@ -31,6 +31,7 @@
 	<div class="loader"></div>
 </div>
 	<div class="navbar navbar-inverse navbar-fixed-top">
+	<div class="card">
 		<div class="container-fluid">	
 			<div class="navbar-header">
 				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse" aria-expanded="false">
@@ -53,14 +54,60 @@
 			    <li><a href="#">&nbsp;En | বাং</a></li>
 				<li><a href="phome.php"><span class="glyphicon glyphicon-home"></span>&nbsp;Product</a></li>				
 			    <li><a href="contact_us.php"><span class="glyphicon glyphicon-envelope"></span>&nbsp;Contact Us</a></li>
+				<li><a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-shopping-cart"></span>&nbsp;Cart&nbsp;<span class="badge">0</span></a>
+					<div class="dropdown-menu" style="width:400px;">
+						<div class="panel panel-success">
+							<div class="panel-heading">
+								<div class="row">
+									<div class="col-md-3">Sl.No</div>
+									<div class="col-md-3">Product Image</div>
+									<div class="col-md-3">Product Name</div>
+									<div class="col-md-3">Price in Tk.</div>
+								</div>
+							</div>
+							<div class="panel-body">
+								<div id="cart_product">
+								
+								</div>
+							</div>
+							<div class="panel-footer"></div>
+						</div>
+					</div>
+				</li>
+				<li><a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user"></span>&nbsp;SignIn</a>
+					<ul class="dropdown-menu">
+						<div style="width:300px;">
+							<div class="panel panel-primary">
+								<div class="panel-heading">Login</div>
+								<div class="panel-heading">
+									<form onsubmit="return false" id="login">
+										<label for="email">Email</label>
+										<input type="email" class="form-control" name="email" id="email" required/>
+										<label for="email">Password</label>
+										<input type="password" class="form-control" name="password" id="password" required/>
+										<p><br/></p>
+										<a href="#" style="color:white; list-style:none;">Forgotten Password</a><input type="submit" class="btn btn-success" style="float:right;">
+									</form>
+								</div>
+								<div class="panel-footer" id="e_msg"></div>
+							</div>
+						</div>
+					</ul>
+				</li>
+			</ul>
 		</div>
 	</div>
-	</div>
+	
+ </div>
+</div> 
+	
 	<p><br/></p>
 	<p><br/></p>
 	<p><br/></p>
 
 
+	
+	
 <!----------------------------------------contact us------------------------------------------------------>
 
                 <div class="panel panel-info">
@@ -86,6 +133,7 @@
 							<img style="float:right; padding:40px;" src="slide_images/customer.jpg" class="img-responsive img-thumbnail" draggable="false" />
 					        </div>
 							<div class="col-md-6">
+							<br>
 							<img style="float:left; padding:10px;" src="slide_images/email.jpg" class="img-responsive img-thumbnail" draggable="false" />
                             <img style="float:left; padding:10px;" src="slide_images/phone.jpg" class="img-responsive img-thumbnail" draggable="false" />							
 							<img style="float:left; padding:10px;" src="slide_images/facebook.jpg" class="img-responsive img-thumbnail" draggable="false" />
@@ -100,7 +148,7 @@
 			</div>
 		</div>	
 					
-					
+	
 					
 
 <!-- //contact -->
@@ -122,7 +170,8 @@
   <li><a href="formalin.php" style="text-decoration:none">Live formalin detection</a></li></li>
   <li><a href="contact_us.php" style="text-decoration:none">Contact Us</a></li></li>
   <li><a href="faq.php" style="text-decoration:none">FAQ</a></li></li>
-  <li><a href="about_us.html" style="text-decoration:none">About Us</a></li></li>
+  <li><a href="about_us.php" style="text-decoration:none">About Us</a></li></li>
+  <li><a href="Supplier_Corporate.php" style="text-decoration:none">Career</a></li></li>
   </br>
   </div>
    <div class="col-md-3" >
@@ -145,6 +194,9 @@
   <div class="panel-footer"><center><img src="copyr.png" height="15px" width="15px">&nbsp;<img src="freshyFood.ico" height="15px" width="15px">&nbsp;Freshy Food&nbsp;All rights reserved</center></div>
 </div>
 
+	<!------------------live chat--------------------->
+	<?php include 'chat.php';?>
+	<!------------------//live chat---------------------> 
 	
 </body>
 </html>
