e0d24e3a98ce3a0c31c4904a2a35376e1d516b47
mayesha-mukarrama <39920277+mayesha-mukarrama@users.noreply.github.com>
2018-09-01 17:15:53 +0600

    Payment page

---
+++ Freshy Food/payment_success.php
@@ -0,0 +1,193 @@
+<?php
+
+session_start();
+if(!isset($_SESSION["uid"])){
+	header("location:index.php");
+}
+
+if (isset($_GET["st"])) {
+
+	# code...
+	$trx_id = $_GET["tx"];
+		$p_st = $_GET["st"];
+		$amt = $_GET["amt"];
+		$cc = $_GET["cc"];
+		$cm_user_id = $_GET["cm"];
+		$c_amt = $_COOKIE["ta"];
+	if ($p_st == "Completed") {
+
+		
+
+		include_once("db.php");
+		$sql = "SELECT p_id,qty FROM cart WHERE user_id = '$cm_user_id'";
+		$query = mysqli_query($con,$sql);
+		if (mysqli_num_rows($query) > 0) {
+			# code...
+			while ($row=mysqli_fetch_array($query)) {
+			$product_id[] = $row["p_id"];
+			$qty[] = $row["qty"];
+			}
+
+			for ($i=0; $i < count($product_id); $i++) { 
+				$sql = "INSERT INTO orders (user_id,product_id,qty,trx_id,p_status) VALUES ('$cm_user_id','".$product_id[$i]."','".$qty[$i]."','$trx_id','$p_st')";
+				mysqli_query($con,$sql);
+			}
+
+			$sql = "DELETE FROM cart WHERE user_id = '$cm_user_id'";
+			if (mysqli_query($con,$sql)) {
+				?>
+					<!DOCTYPE html>
+					<html>
+						<head>
+							<meta charset="UTF-8">
+							<title>Freshy Food</title>
+							<link rel="stylesheet" href="css/bootstrap.min.css"/>
+							<script src="js/jquery2.js"></script>
+							<script src="js/bootstrap.min.js"></script>
+							<script src="main.js"></script>
+							<link rel="shortcut icon" href="freshyFood.ico">
+							<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript">
+$(function() {
+    $(this).bind("contextmenu", function(e) {
+        e.preventDefault();
+    });
+}); 
+</script>
+<script type="text/JavaScript"> 
+    function killCopy(e){ return false } 
+    function reEnable(){ return true } 
+    document.onselectstart=new Function ("return false"); 
+    if (window.sidebar)
+    { 
+        document.onmousedown=killCopy; 
+        document.onclick=reEnable; 
+    } 
+	
+document.onkeydown = function(e) {
+if(event.keyCode == 123) {
+return false;
+}
+if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
+return false;
+}
+if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
+return false;
+}
+if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
+return false;
+}
+}
+</script>
+							<style>
+								table tr td {padding:10px;}
+							</style>
+						</head>
+					<body oncontextmenu="return false;">
+						<div class="navbar navbar-inverse navbar-fixed-top">
+						
+						
+	<!------------------live chat--------------------->
+	<?php include 'chat.php';?>
+	<!------------------//live chat--------------------->
+	
+							<div class="container-fluid">	
+								<div class="navbar-header">
+									<a href="index.php" class="navbar-brand" style="font-family:Jokerman;color:white">FreshyFood</a>
+			</div>
+		<div class="collapse navbar-collapse" id="collapse">
+			<ul class="nav navbar-nav">
+				 
+				<li style="width:230px;left:10px;top:10px;"><input type="text" class="form-control" id="search" placeholder="Search"></li>
+				<li style="top:10px;left:20px;"><button class="btn btn-primary" id="search_btn">Search</button></li>
+			</ul>
+			
+			<ul class="nav navbar-nav navbar-right">
+			 
+			    <li><a href="#">&nbsp;En | বাং</a></li>
+				<li><a href="phome.php"><span class="glyphicon glyphicon-home"></span>&nbsp;Product</a></li>				
+			    <li><a href="#contact_us"><span class="glyphicon glyphicon-envelope"></span>&nbsp;Contact Us</a></li>
+				</div>
+							</div>
+						</div>
+						<p><br/></p>
+						<p><br/></p>
+						<p><br/></p>
+						<div class="container-fluid">
+						
+							<div class="row">
+								<div class="col-md-2"></div>
+								<div class="col-md-8">
+									<div class="panel panel-default">
+										<div class="panel-heading"></div>
+										<div class="panel-body">
+											<h1>Thankyou for Shopping</h1>
+											<hr/>
+											<p>Hello <?php echo "<b>".$_SESSION["name"]."</b>"; ?>,Your payment process is 
+											successfully completed and your Transaction id is <b><?php echo $trx_id; ?></b><br/>
+											you can continue your Shopping <br/></p>
+											<a href="phome.php" class="btn btn-success btn-lg">Continue Shopping</a>
+										</div>
+										
+									</div>
+								</div>
+								<div class="col-md-2"></div>
+							</div>
+						</div>
+					</body>
+					</html>
+
+				<?php
+			}
+		}else{
+			header("location:index.php");
+		}
+		
+	}
+}
+?>
+    <p><br/></p>
+	<p><br/></p>
+	<p><br/></p>
+				<!-------------------------------------footer------------------------------------------------------->
+
+
+<div class="card">
+
+<div class="panel-footer">
+								
+  <ul style="list-style-type:none" >
+  <center>
+  <div class="row">
+			
+    <div class="col-md-3" >
+  <li><style="text-decoration:none"><h4>Our Services:</h4></li>
+  <li><a href="formalin.php" style="text-decoration:none">Live formalin detection</a></li></li>
+  <li><a href="contact_us.php" style="text-decoration:none">Contact Us</a></li></li>
+  <li><a href="faq.php" style="text-decoration:none">FAQ</a></li></li>
+  <li><a href="about_us.php" style="text-decoration:none">About Us</a></li></li>
+  </br>
+  </div>
+   <div class="col-md-3" >
+  <li><style="text-decoration:none"><h4>Our Products:</h4></li>
+  <li><a href="phome.php" style="text-decoration:none">Fruits</a></li>
+  <li><a href="phome.php" style="text-decoration:none">Vegetables</a></li>
+  <li><a href="phome.php" style="text-decoration:none">Baverages</a></li>
+  <li><a href="phome.php" style="text-decoration:none">Gift Basket</a></li>
+  <li><a href="phome.php" style="text-decoration:none">Dairy</a></li>
+  <li><a href="phome.php" style="text-decoration:none">Packages</a></li>
+  </br>
+  </div>
+  <div class="col-md-6" >
+   <li><style="text-decoration:none"><h4>Payment Options:</h4></li></br>
+  <li><span style="font-size: 10px; font-weight:bold; display: inline-flex;vertical-align: super; width: 50px">CASH ON DELIVERY</span>&nbsp;&nbsp;<img src="slide_images/bkash.png">&nbsp;&nbsp;<img src="slide_images/rocket.png">&nbsp;&nbsp;<img src="slide_images/ucash.png">  </li>
+  </center>
+  </div>
+  </div>
+  </div>
+  <div class="panel-footer"><center><img src="copyr.png" height="15px" width="15px">&nbsp;<img src="freshyFood.ico" height="15px" width="15px">&nbsp;Freshy Food&nbsp;All rights reserved</center></div>
+</div>
+
+	
+</body>
+</html>
