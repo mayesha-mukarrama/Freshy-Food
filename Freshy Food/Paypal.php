51cab839fcdbe888ddafd79afc0e502af4df6dce
DaizySultana <31015298+DaizySultana@users.noreply.github.com>
2018-09-02 11:11:31 +0600

    Add files via upload

---
+++ Freshy Food/Paypal.php
@@ -0,0 +1,113 @@
+﻿<?php
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>FreshyFood</title>
+		<link rel="stylesheet" href="css/bootstrap.min.css"/>
+		<script src="js/jquery2.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="main.js"></script>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<link rel="shortcut icon" href="freshyFood.ico">
+		<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript">
+$(function() {
+    $(this).bind("contextmenu", function(e) {
+        e.preventDefault();
+    });
+}); 
+</script>
+<script type="text/JavaScript"> 
+    function killCopy(e){ return false } 
+    function reEnable(){ return true } 
+    document.onselectstart=new Function ("return false"); 
+    if (window.sidebar)
+    { 
+        document.onmousedown=killCopy; 
+        document.onclick=reEnable; 
+    } 
+	
+
+</script>
+	</head>
+<body oncontextmenu="return false;">
+<div class="wait overlay">
+	<div class="loader"></div>
+</div>
+	<div class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container-fluid">	
+			<div class="navbar-header">
+				<a href="index.php" class="navbar-brand" style="font-family:Jokerman;color:white">FreshyFood</a>
+			</div>
+		<div class="collapse navbar-collapse" id="collapse">
+			<ul class="nav navbar-nav">
+				 
+				<li style="width:230px;left:10px;top:10px;"><input type="text" class="form-control" id="search" placeholder="Search"></li>
+				<li style="top:10px;left:20px;"><button class="btn btn-primary" id="search_btn">Search</button></li>
+			</ul>
+			
+			<ul class="nav navbar-nav navbar-right">
+			 
+			    <li><a href="#">&nbsp;En | বাং</a></li>
+				<li><a href="phome.php"><span class="glyphicon glyphicon-home"></span>&nbsp;Product</a></li>				
+			    <li><a href="#"><span class="glyphicon glyphicon-envelope"></span>&nbsp;Contact Us</a></li>
+				</div>
+		</div>
+	</div>
+	<p><br/></p>
+	<p><br/></p>
+	<p><br/></p>
+	
+		<div class="row">
+			<div class="col-md-4"></div>
+			<div class="col-md-4">
+				<div class="panel panel-primary">
+					<div class="panel-heading">Payment With Paypal</div>
+					<div class="panel-body">
+						<div class="payment-info">
+		     <h4>Already Have A PayPal Account?</h4>
+		 <div class="login-tab">
+		<div class="user-login">
+													<center><h2>Login</h2></center>
+													
+						
+						<form onsubmit="return false" id="submit">
+							<label for="email">MAIL</label>
+							<input type="email" class="form-control" name="email" id="email" required/>
+							<label for="passward">PASSWARD</label>
+							<input type="passward" class="form-control" name="passward" id="passward" required/>
+						        <input type="checkbox" id="brand" value="">
+							<label for="brand"><span></span>Remember me?</label>
+<div class="single-bottom">
+					
+											</div>
+							<p><br/></p>
+                                                           <center><INPUT TYPE = "Submit" Name = "Submit1" VALUE = "Submit"></center>
+													
+						</form>
+			
+				</div>
+				<div class="panel-footer"><center><img src="copyr.png" height="15px" width="15px">&nbsp;<img src="freshyFood.ico" height="15px" width="15px">&nbsp;FreshyFood&nbsp;All rights reserved</center><div id="e_msg"></div></div>
+			</div>
+		</div>
+		<div class="col-md-4"></div>
+	</div>
+	
+	 <p><br/></p>
+	<p><br/></p>
+	<p><br/></p>
+				
+
+
+
+
+
+
+
+
+
+
+
+
