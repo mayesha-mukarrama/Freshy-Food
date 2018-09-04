99a46d045306857df0196eb7ddfa4d8b03bad7e1
faiza123 <fairin123@gmail.com>
2018-09-04 22:28:02 +0600

    supplier-new

--- Freshy Food/Supplier/change-password.php
+++ Freshy Food/Supplier/change-password.php
@@ -1,4 +1,4 @@
-
+﻿
 <?php
 session_start();
 include('include/config.php');
@@ -7,7 +7,7 @@ if(strlen($_SESSION['alogin'])==0)
 header('location:index.php');
 }
 else{
-date_default_timezone_set('Asia/Kolkata');// change according timezone
+date_default_timezone_set('Asia/Bangladesh');// change according timezone
 $currentTime = date( 'd-m-Y h:i:s A', time () );
 
 
@@ -27,62 +27,91 @@ $_SESSION['msg']="Old Password not match !!";
 }
 ?>
 <!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Admin| Change Password</title>
-	<link type="text/css" href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-	<link type="text/css" href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
-	<link type="text/css" href="css/theme.css" rel="stylesheet">
-	<link type="text/css" href="images/icons/css/font-awesome.css" rel="stylesheet">
-	<link type="text/css" href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600' rel='stylesheet'>
-	<link rel="shortcut icon" href="../freshyfood.ico">
-	<script type="text/javascript">
-function valid()
-{
-if(document.chngpwd.password.value=="")
-{
-alert("Current Password Filed is Empty !!");
-document.chngpwd.password.focus();
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>FreshyFood</title>
+		<link rel="stylesheet" href="css/bootstrap.min.css"/>
+		<script src="js/jquery2.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="main.js"></script>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<link rel="shortcut icon" href="freshyFood.ico">
+		<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript">
+$(function() {
+    $(this).bind("contextmenu", function(e) {
+        e.preventDefault();
+    });
+}); 
+</script>
+<script type="text/JavaScript"> 
+    function killCopy(e){ return false } 
+    function reEnable(){ return true } 
+    document.onselectstart=new Function ("return false"); 
+    if (window.sidebar)
+    { 
+        document.onmousedown=killCopy; 
+        document.onclick=reEnable; 
+    } 
+	
+document.onkeydown = function(e) {
+if(event.keyCode == 123) {
 return false;
 }
-else if(document.chngpwd.newpassword.value=="")
-{
-alert("New Password Filed is Empty !!");
-document.chngpwd.newpassword.focus();
+if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
 return false;
 }
-else if(document.chngpwd.confirmpassword.value=="")
-{
-alert("Confirm Password Filed is Empty !!");
-document.chngpwd.confirmpassword.focus();
+if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
 return false;
 }
-else if(document.chngpwd.newpassword.value!= document.chngpwd.confirmpassword.value)
-{
-alert("Password and Confirm Password Field do not match  !!");
-document.chngpwd.confirmpassword.focus();
+if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
 return false;
 }
-return true;
 }
 </script>
-
-<style>
-	
-	.card {
-    box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
-    transition: all 0.3s cubic-bezier(.25,.8,.25,1);
-    width: 100%;
-}
-
-.card:hover {
-     box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
-}
-	</style>
 </head>
-<body>
+<body oncontextmenu="return false;">
+<div class="wait overlay">
+	<div class="loader"></div>
+</div>
+	<div class="navbar navbar-inverse navbar-fixed-top">
+		<div class="container-fluid">	
+			<div class="navbar-header">
+				<a href="index.php" class="navbar-brand" style="font-family:Jokerman;color:white">FreshyFood</a>
+			</div>
+		<div class="collapse navbar-collapse" id="collapse">
+			<ul class="nav navbar-nav">
+				 
+				<li style="width:230px;left:10px;top:10px;"><input type="text" class="form-control" id="search" placeholder="Search"></li>
+				<li style="top:10px;left:20px;"><button class="btn btn-primary" id="search_btn">Search</button></li>
+			</ul>
+			
+			<ul class="nav navbar-nav navbar-right">
+			 
+			    <li><a href="#">&nbsp;En |  বাং</a></li>
+				<li><a href="phome.php"><span class="glyphicon glyphicon-home"></span>&nbsp;Product</a></li>				
+			    <li><a href="#"><span class="glyphicon glyphicon-envelope"></span>&nbsp;Contact Us</a></li>
+				</div>
+		</div>
+	</div>
+	<p><br/></p>
+	<p><br/></p>
+	<p><br/></p>
+<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-2"></div>
+			<div class="col-md-8" id="signup_msg">
+				<!--Alert from signup form-->
+			</div>
+			<div class="col-md-2"></div>
+		</div>
+		<div class="row">
+			<div class="col-md-4"></div>
+			<div class="col-md-4">
+				<div class="panel panel-primary">
+	
+
 <?php include('include/header.php');?>
 
 	<div class="wrapper">
@@ -90,15 +119,17 @@ return true;
 		
 			<div class="row">
 <?php include('include/sidebar.php');?>				
-			<div class="span9">
-					<div class="content">
-
-						<div class="module">
-						   <div class="card">
-							<div class="module-head">
-								<h3>Admin Change Password</h3>
-							</div>
-							<div class="module-body">
+			<div class="panel-heading">Supplier/Vendor Profile Form</div>
+					<div class="panel-body">
+						<!--User Login Form-->
+						<form onsubmit="return false" id="password">
+							<label for="currentpassword">Current Password</label>
+							<input type="password" class="form-control" name="password" id="password" required/>
+							<label for="ID"> New Password</label>
+							<input type="password" class="form-control" name="password" id="password" required/>
+<label for="ID"> Confirm Password</label>
+							<input type="password" class="form-control" name="password" id="password" required/>
+							<p><br/></p>
 
 									<?php if(isset($_POST['submit']))
 {?>
@@ -157,11 +188,69 @@ return true;
 		</div><!--/.container-->
 	</div><!--/.wrapper-->
 
-<?php include('include/footer.php');?>
+div class="panel-footer"><center><img src="copyr.png" height="15px" width="15px">&nbsp;<img src="freshyFood.ico" height="15px" width="15px">&nbsp;FreshyFood&nbsp;All rights reserved</center><div id="e_msg"></div></div>
+			</div>
+		</div>
+		<div class="col-md-4"></div>
+	</div>
+	
+	 <p><br/></p>
+	<p><br/></p>
+	<p><br/></p>
 
-	<script src="scripts/jquery-1.9.1.min.js" type="text/javascript"></script>
-	<script src="scripts/jquery-ui-1.10.1.custom.min.js" type="text/javascript"></script>
-	<script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-	<script src="scripts/flot/jquery.flot.js" type="text/javascript"></script>
+
+				 <!-------------------------------------footer------------------------------------------------------->
+
+
+<div class="card">
+
+<div class="panel-footer">
+								
+  <ul style="list-style-type:none" >
+  <center>
+  <div class="row">
+			
+    <div class="col-md-3" >
+  <li><style="text-decoration:none"><h4>Our Services:</h4></li>
+  <li><a href="formalin.php" style="text-decoration:none">Live formalin detection</a></li></li>
+  <li><a href="contact_us.php" style="text-decoration:none">Contact Us</a></li></li>
+  <li><a href="faq.php" style="text-decoration:none">FAQ</a></li></li>
+  <li><a href="about_us.php" style="text-decoration:none">About Us</a></li></li>
+  </br>
+  </div>
+   <div class="col-md-3" >
+  <li><style="text-decoration:none"><h4>Our Products:</h4></li>
+  <li><a href="phome.php" style="text-decoration:none">Fruits</a></li>
+  <li><a href="phome.php" style="text-decoration:none">Vegetables</a></li>
+  <li><a href="phome.php" style="text-decoration:none">Baverages</a></li>
+  <li><a href="phome.php" style="text-decoration:none">Gift Basket</a></li>
+  <li><a href="phome.php" style="text-decoration:none">Dairy</a></li>
+  <li><a href="phome.php" style="text-decoration:none">Packages</a></li>
+  </br>
+  </div>
+  <div class="col-md-6" >
+   <li><style="text-decoration:none"><h4>Payment Options:</h4></li></br>
+  <li><span style="font-size: 10px; font-weight:bold; display: inline-flex;vertical-align: super; width: 50px">CASH ON DELIVERY</span>&nbsp;&nbsp;<img src="slide_images/bkash.png">&nbsp;&nbsp;<img src="slide_images/rocket.png">&nbsp;&nbsp;<img src="slide_images/ucash.png">  </li>
+  </center>
+  </div>
+  </div>
+  </div>
+  <div class="panel-footer"><center><img src="copyr.png" height="15px" width="15px">&nbsp;<img src="freshyFood.ico" height="15px" width="15px">&nbsp;Freshy Food&nbsp;All rights reserved</center></div>
+</div>
+
+	
 </body>
+</html>
 <?php } ?>
+
+
+
+
+
+
+
+
+
+
+
+
