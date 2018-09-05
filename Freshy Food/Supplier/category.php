8e043b1a73d18edb261b8fc592489a1ddc307d50
faiza123 <fairin123@gmail.com>
2018-09-06 04:42:43 +0600

    supplier

---
+++ Freshy Food/Supplier/category.php
@@ -0,0 +1,186 @@
+﻿
+<?php
+session_start();
+include('include/config.php');
+if(strlen($_SESSION['alogin'])==0)
+	{	
+header('location:index.php');
+}
+else{
+date_default_timezone_set('Asia/Dhaka');// change according timezone
+$currentTime = date( 'd-m-Y h:i:s A', time () );
+
+
+if(isset($_POST['submit']))
+{
+	$category=$_POST['category'];
+	$description=$_POST['description'];
+$sql=mysql_query("insert into categories(cat_id,cat_title) values('$category','$description')");
+$_SESSION['msg']="Category Created !!";
+
+}
+
+if(isset($_GET['del']))
+		  {
+		          mysql_query("delete from categories where cat_id= '".$_GET['id']."'");
+                  $_SESSION['delmsg']="Category deleted !!";
+		  }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Supplier| Category</title>
+	<link type="text/css" href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link type="text/css" href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
+	<link type="text/css" href="css/theme.css" rel="stylesheet">
+	<link type="text/css" href="images/icons/css/font-awesome.css" rel="stylesheet">
+	<link type="text/css" href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600' rel='stylesheet'>
+	<link rel="shortcut icon" href="../freshyfood.ico">
+		<style>
+	
+	.card {
+    box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+    transition: all 0.3s cubic-bezier(.25,.8,.25,1);
+    width: 100%;
+}
+
+.card:hover {
+     box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+}
+	</style>
+</head>
+<body>
+<?php include('include/header.php');?>
+
+	<div class="wrapper">
+		<div class="container">
+			<div class="row">
+<?php include('include/sidebar.php');?>				
+			<div class="span9">
+					<div class="content">
+
+						<div class="module">
+						<div class="card">
+							<div class="module-head">
+								<h3>Category</h3>
+							</div>
+							<div class="module-body">
+
+									<?php if(isset($_POST['submit']))
+{?>
+									<div class="alert alert-success">
+										<button type="button" class="close" data-dismiss="alert">×</button>
+									<strong>Well done!</strong>	<?php echo htmlentities($_SESSION['msg']);?><?php echo htmlentities($_SESSION['msg']="");?>
+									</div>
+<?php } ?>
+
+
+									<?php if(isset($_GET['del']))
+{?>
+									<div class="alert alert-error">
+										<button type="button" class="close" data-dismiss="alert">×</button>
+									<strong>Oh snap!</strong> 	<?php echo htmlentities($_SESSION['delmsg']);?><?php echo htmlentities($_SESSION['delmsg']="");?>
+									</div>
+<?php } ?>
+
+									<br />
+
+			<form class="form-horizontal row-fluid" name="Category" method="post" >
+									
+<div class="control-group">
+<label class="control-label" for="basicinput">Category ID</label>
+<div class="controls">
+<input type="text" placeholder="Enter category ID"  name="category" class="span8 tip" required>
+</div>
+</div>
+
+
+<div class="control-group">
+											<label class="control-label" for="basicinput">Category Title</label>
+											<div class="controls">
+												<input type="text" placeholder="Enter category title"  name="description" class="span8 tip" required>
+											</div>
+										</div>
+
+	<div class="control-group">
+											<div class="controls">
+												<button type="submit" name="submit" class="btn">Create</button>
+											</div>
+										</div>
+									</form>
+							</div>
+							</div>
+						</div>
+
+
+	<div class="module">
+	   <div class="card">
+							<div class="module-head">
+								<h3>Manage Categories</h3>
+							</div>
+							<div class="module-body table">
+								<table cellpadding="0" cellspacing="0" border="0" class="datatable-1 table table-bordered table-striped	 display" width="100%">
+									<thead>
+										<tr>
+								
+										    <th>Category ID</th>
+											<th>Category Title</th>
+											<th>Creation date</th>
+											<th>Last Updated</th>
+											<th>Action</th>
+										
+										</tr>
+									</thead>
+									<tbody>
+
+<?php $query=mysql_query("select * from categories ");
+$cnt=1;
+while($row=mysql_fetch_array($query))
+{
+?>									
+										<tr>
+				
+											<td><?php echo htmlentities($row['cat_id']);?></td>
+											<td><?php echo htmlentities($row['cat_title']);?></td>
+											<td> <?php echo htmlentities($row['creationDate']);?></td>
+											<td><?php echo htmlentities($row['updationDate']);?></td>
+											<td>
+											<a href="edit-category.php?id=<?php echo $row['cat_id']?>" ><i class="icon-edit"></i></a>
+											<a href="category.php?id=<?php echo $row['cat_id']?>&del=delete" onClick="return confirm('Are you sure you want to delete?')"><i class="icon-remove-sign"></i></a></td>
+										</tr>
+										<?php $cnt=$cnt+1; } ?>
+										
+								</table>
+							</div>
+							</div>
+						</div>						
+
+						
+						
+					</div><!--/.content-->
+				</div><!--/.span9-->
+			</div>
+		</div><!--/.container-->
+	</div><!--/.wrapper-->
+
+<?php include('include/footer.php');?>
+
+	<script src="scripts/jquery-1.9.1.min.js" type="text/javascript"></script>
+	<script src="scripts/jquery-ui-1.10.1.custom.min.js" type="text/javascript"></script>
+	<script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="scripts/flot/jquery.flot.js" type="text/javascript"></script>
+	<script src="scripts/datatables/jquery.dataTables.js"></script>
+	<script>
+		$(document).ready(function() {
+			$('.datatable-1').dataTable();
+			$('.dataTables_paginate').addClass("btn-group datatable-pagination");
+			$('.dataTables_paginate > a').wrapInner('<span />');
+			$('.dataTables_paginate > a:first-child').append('<i class="icon-chevron-left shaded"></i>');
+			$('.dataTables_paginate > a:last-child').append('<i class="icon-chevron-right shaded"></i>');
+		} );
+	</script>
+</body>
+<?php } ?>
